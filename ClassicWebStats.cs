a514d56ceb61f527dce41dd526fbd9d2c2f40d36
ZacAttack <dexrn@hotmail.com>
2024-10-05 18:43:10 -0700

    expose more stats

--- ClassicWebStats.cs
+++ ClassicWebStats.cs
@@ -11,6 +11,7 @@
 //reference System.Core.dll
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -25,29 +26,95 @@ namespace ClassicWebStats
         public static JsonArray Players { get; set; }
         public static string Started { get; set; }
         public static string Uptime { get; set; }
-        public static string MainMap { get; set; }
+        public static JsonObject MainMap { get; set; }
     }
 
     public class CStatsWebServer
     {
-        private static HttpListener listener = new System.Net.HttpListener();
-        private static bool isRunning = false;
+        private static readonly HttpListener listener = new HttpListener();
+        public static string pluginName = "";
+        public static string pluginCreator = "";
+        public static string pluginVersion = "";
+
+        private static JsonArray GetPlayersStats(List<Player> players)
+        {
+            JsonArray playerArray = new JsonArray();
+            foreach (var player in players)
+            {
+                TimeSpan joinTime = DateTime.UtcNow.Subtract(player.SessionStartTime);
+                playerArray.Add(new JsonObject {
+                    { "Name", new JsonObject {
+                        { "Name", player.name },
+                        { "FullName", player.FullName },
+                        { "DisplayName", player.DisplayName },
+                        { "ColoredDisplayName", player.ColoredName },
+                        { "Prefix", player.prefix },
+                        { "Title", player.title },
+                        { "TitleColor", player.titlecolor }
+                    }},
+                    { "AFK", player.IsAfk },
+                    { "LastActionTime", player.LastAction.ToUnixTime().ToString() },
+                    { "Group", player.group.Name },
+                    { "GroupColored", player.group.ColoredName },
+                    { "Rank", player.Rank.ToString() },
+                    { "Client", player.Session.ClientName() },
+                    { "Skin", player.SkinName },
+                    { "JoinTime", player.SessionStartTime.ToUnixTime().ToString() },
+                    { "OnlineTime", string.Format("{0:D2}:{1:D2}:{2:D2}", joinTime.Hours, joinTime.Minutes, joinTime.Seconds) },
+                    { "Map", player.level.name },
+                    { "Coordinates", new JsonObject {
+                        { "Block", new JsonObject {
+                            { "X", player.Pos.FeetBlockCoords.X },
+                            { "Y", player.Pos.FeetBlockCoords.Y },
+                            { "Z", player.Pos.FeetBlockCoords.Z }
+                        }},
+                        { "Unit", new JsonObject {
+                            { "X", player.Pos.X },
+                            { "Y", player.Pos.Y },
+                            { "Z", player.Pos.Z }
+                        }}
+                    }},
+                    { "HeldBlock", new JsonObject {
+                        { "Name", Block.GetName(player, player.ClientHeldBlock) },
+                        { "Id", (int)player.ClientHeldBlock }
+                    }},
+                });
+            };
+            return playerArray;
+        }
 
         private static void GetCurrentStats()
         {
             // get players online
             Player fakePlayer = new Player("Test");
-            Stats.Players = new JsonArray();
-            foreach (var player in PlayerInfo.GetOnlineCanSee(fakePlayer, LevelPermission.Guest).Select(player => player.DisplayName).ToList())
-            {
-                Stats.Players.Add(player);
-            };
+            Stats.Players = GetPlayersStats(PlayerInfo.GetOnlineCanSee(fakePlayer, LevelPermission.Guest).ToList());
             // get player count
             Stats.PlayerCount = PlayerInfo.Online.Count;
             Stats.Started = Server.StartTime.ToUnixTime().ToString();
-            TimeSpan uptime = DateTime.UtcNow.Subtract(MCGalaxy.Server.StartTime);
+            TimeSpan uptime = DateTime.UtcNow.Subtract(Server.StartTime);
             Stats.Uptime = string.Format("{0:D2}:{1:D2}:{2:D2}", uptime.Hours, uptime.Minutes, uptime.Seconds);
-            Stats.MainMap = Server.mainLevel.name;
+            Stats.MainMap = new JsonObject
+            {
+                { "Name", Server.mainLevel.name },
+                { "Players", GetPlayersStats(Server.mainLevel.players) },
+                { "Bounds", new JsonObject {
+                    { "Width", (int)Server.mainLevel.Width },
+                    { "Depth", (int)Server.mainLevel.Length },
+                    { "Height", (int)Server.mainLevel.Height }
+                }},
+                { "SpawnPos", new JsonObject {
+                    { "Block", new JsonObject {
+                        { "X", Server.mainLevel.SpawnPos.FeetBlockCoords.X },
+                        { "Y", Server.mainLevel.SpawnPos.FeetBlockCoords.Y },
+                        { "Z", Server.mainLevel.SpawnPos.FeetBlockCoords.Z }
+                    }},
+                    { "Unit", new JsonObject {
+                        { "X", Server.mainLevel.SpawnPos.X },
+                        { "Y", Server.mainLevel.SpawnPos.Y },
+                        { "Z", Server.mainLevel.SpawnPos.Z }
+                    }}
+                }}
+            };
         }
 
         public static void StartWebServer()
@@ -55,9 +122,8 @@ namespace ClassicWebStats
             string url = "http://localhost:8081/";
             listener.Prefixes.Add(url);
             listener.Start();
-            isRunning = true;
             Logger.Log(LogType.Debug, "Listening on {0}", url);
-            while (isRunning)
+            while (listener.IsListening)
             {
                 GetCurrentStats();
                 var stats = new JsonObject
@@ -66,12 +132,13 @@ namespace ClassicWebStats
                     {  "Players", Stats.Players },
                     {  "Started", Stats.Started },
                     {  "Uptime", Stats.Uptime },
-                    {  "MainMapName", Stats.MainMap },
+                    {  "MainMap", Stats.MainMap },
                 };
 
                 HttpListenerContext context = listener.GetContext();
                 HttpListenerRequest request = context.Request;
                 HttpListenerResponse response = context.Response;
+                response.Headers.Add("Server", pluginName + " " + pluginVersion + " on " + Server.SoftwareNameVersioned + ";");
                 response.ContentType = "application/json";
 
                 string json = Json.SerialiseObject(stats);
@@ -87,10 +154,8 @@ namespace ClassicWebStats
         {
             if (listener.IsListening)
             {
-                isRunning = false;
                 listener.Stop();
             }
-                
         }
 
     }
@@ -99,10 +164,13 @@ namespace ClassicWebStats
     {
         public override string name { get { return "Classic Web Stats"; } }
         public override string creator { get { return "DexrnZacAttack"; } }
-        public override string MCGalaxy_Version { get { return "1.0.0.0"; } }
+        public override string MCGalaxy_Version { get { return "1.1.0.0"; } }
 
         public override void Load(bool startup)
         {
+            CStatsWebServer.pluginName = name;
+            CStatsWebServer.pluginCreator = creator;
+            CStatsWebServer.pluginVersion = MCGalaxy_Version;
             CStatsWebServer.StartWebServer();
             Logger.Log(LogType.Debug, "{0} {1} loaded.", name, MCGalaxy_Version);
         }
